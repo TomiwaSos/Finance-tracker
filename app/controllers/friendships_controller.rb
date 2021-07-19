a8538818aef1cd88de66d79786cd0353274518ce
Tomiwa Sosanya <tomiwa@doyoucustoms.com>
2021-07-19 13:59:18 +0100

    added backend functionaility of search for friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,4 +4,25 @@ class FriendshipsController < ApplicationController
     @friendships = current_user.friends
     end
 
+    def search
+        if params[:friend].present?
+            @friend = params[:friend]
+            if @friend
+                respond_to do |format|
+                    format.js { render partial: 'friendships/result' }
+                end
+            else
+                respond_to do |format|
+                    flash.now[:alert] = "please enter in a friend to search"
+                    format.js { render partial: 'friendships/result' }
+                end
+            end
+        else 
+            respond_to do |format|
+                flash.now[:alert] = "please enter in a friend to search"
+                format.js { render partial: 'friendships/result' }
+            end
+        end
+    end
+
 end
